8ffa3fbdeeea11db63edd9d44f177333f3fc3680
Gerald Imanuel <gerald.imanuel@student.umn.ac.id>
2022-07-12 18:44:26 +0700

    fixing mainpage ui

--- src/components/MatchHistory.jsx
+++ src/components/MatchHistory.jsx
@@ -1,13 +1,22 @@
-import { Heading, Flex, Text, Box, Image } from "@chakra-ui/react";
+import {
+	Heading,
+	Flex,
+	Text,
+	Box,
+	Image,
+	useColorModeValue,
+} from "@chakra-ui/react";
 
 export default function MatchHistory() {
+	const bg = useColorModeValue("#FFFFFF", "#121212");
 	return (
 		<Flex
-			bgColor="white"
+			bgColor={bg}
 			borderRadius="20px"
 			className="current-rank"
 			alignItems="center"
 			w="100%"
+			mb={{ base: 5, md: 0 }}
 		></Flex>
 	);
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,43 +7,44 @@ import Rank from "../components/Rank";
 import MatchHistory from "../components/MatchHistory";
 
 export default function MainPage() {
-  const bg = useColorModeValue("#F4F6F8", "#2C2C2C");
+	const bg = useColorModeValue("#F4F6F8", "#2C2C2C");
 
-  return (
-    <Flex maxW="2000px" h="100vh" overflow="hidden">
-      <Navbar />
-      <Flex
-        w={{ md: "96%", base: "87%" }}
-        bgColor={bg}
-        className="main-page"
-        alignItems="center"
-        flexDirection={{ base: "column", md: "row" }}
-      >
-        <Flex p={{ base: 5, md: 0 }} w={{ md: "25%", base: "100%" }}>
-          <Profile />
-        </Flex>
-        <Flex
-          className="tracker-bar"
-          w={{ md: "75%", base: "100%" }}
-          p={{ md: 8, base: null }}
-          px={{ md: 8, base: 5 }}
-          flexDirection="column"
-          rowGap={{ base: 5, md: 0 }}
-        >
-          <Flex
-            h={{ md: "45%", base: "100%" }}
-            columnGap={5}
-            flexDirection={{ md: "row", base: "column" }}
-            rowGap={{ base: 5, md: null }}
-          >
-            <Rank />
-            <Act />
-          </Flex>
-          <Flex h={{ md: "55%", base: "100%" }}>
-            <MatchHistory />
-          </Flex>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex maxW="2000px" h="100vh" overflow={{ md: "hidden", base: "scroll" }}>
+			<Navbar />
+			<Flex
+				w={{ md: "96%", base: "87%" }}
+				bgColor={bg}
+				className="main-page"
+				alignItems="center"
+				flexDirection={{ base: "column", md: "row" }}
+			>
+				<Flex p={{ base: 5, md: 0 }} w={{ md: "25%", base: "100%" }}>
+					<Profile />
+				</Flex>
+				<Flex
+					className="tracker-bar"
+					w={{ md: "75%", base: "100%" }}
+					p={{ md: 8, base: null }}
+					px={{ md: 8, base: 5 }}
+					flexDirection="column"
+					rowGap={{ base: 5, md: 5 }}
+					h="100%"
+				>
+					<Flex
+						h={{ md: "45%", base: "100%" }}
+						columnGap={5}
+						flexDirection={{ md: "row", base: "column" }}
+						rowGap={{ base: 5, md: null }}
+					>
+						<Rank />
+						<Act />
+					</Flex>
+					<Flex h={{ md: "55%", base: "100%" }}>
+						<MatchHistory />
+					</Flex>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
 }

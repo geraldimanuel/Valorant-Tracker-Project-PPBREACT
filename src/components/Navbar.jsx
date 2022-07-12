a9003de937f33fe621318d62e8372f2c1c896ab0
Jefer Setiawan <jefersetiawan04@gmail.com>
2022-07-12 11:20:33 +0700

    prettier

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { React, useState } from "react";
 import {
-	Flex,
-	Box,
-	Button,
-	useColorMode,
-	useColorModeValue,
+  Flex,
+  Box,
+  Button,
+  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 
 import { FaMoon, FaSun } from "react-icons/fa";
@@ -12,48 +12,48 @@ import { MdArrowBackIosNew } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-	const [active, setActive] = useState(false);
-	const { toggleColorMode } = useColorMode();
-	const bg = useColorModeValue("#FFFFFF", "#121212");
-	const icons = useColorModeValue("#121212", "#FFFFFF");
-	const bgComponent = useColorModeValue("#F4F6F8", "#2C2C2C");
+  const [active, setActive] = useState(false);
+  const { toggleColorMode } = useColorMode();
+  const bg = useColorModeValue("#FFFFFF", "#121212");
+  const icons = useColorModeValue("#121212", "#FFFFFF");
+  const bgComponent = useColorModeValue("#F4F6F8", "#2C2C2C");
 
-	function buttonHandle() {
-		setActive(!active);
-		toggleColorMode();
-	}
+  function buttonHandle() {
+    setActive(!active);
+    toggleColorMode();
+  }
 
-	return (
-		<Flex
-			bg={bg}
-			w={{ md: "4%", base: "13%" }}
-			className="left-navbar"
-			alignItems="center"
-			flexDirection="column"
-			rowGap={4}
-		>
-			<Link to="/">
-				<Button
-					color={icons}
-					className="menu-button"
-					bgColor={bgComponent}
-					p={1}
-					borderRadius="8px"
-					mt={2}
-				>
-					<MdArrowBackIosNew size="25px" />
-				</Button>
-			</Link>
-			<Button
-				onClick={buttonHandle}
-				className="darkmode-button"
-				color={icons}
-				bgColor={bgComponent}
-				p={1}
-				borderRadius="8px"
-			>
-				{active ? <FaSun size="25px" /> : <FaMoon size="25px" />}
-			</Button>
-		</Flex>
-	);
+  return (
+    <Flex
+      bg={bg}
+      w={{ md: "4%", base: "13%" }}
+      className="left-navbar"
+      alignItems="center"
+      flexDirection="column"
+      rowGap={4}
+    >
+      <Link to="/">
+        <Button
+          color={icons}
+          className="menu-button"
+          bgColor={bgComponent}
+          p={1}
+          borderRadius="8px"
+          mt={2}
+        >
+          <MdArrowBackIosNew size="25px" />
+        </Button>
+      </Link>
+      <Button
+        onClick={buttonHandle}
+        className="darkmode-button"
+        color={icons}
+        bgColor={bgComponent}
+        p={1}
+        borderRadius="8px"
+      >
+        {active ? <FaSun size="25px" /> : <FaMoon size="25px" />}
+      </Button>
+    </Flex>
+  );
 }
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -8,128 +8,128 @@ import wallpaper from "../assets/wallpaper.jpg";
 import { useNavigate } from "react-router-dom";
 
 export default function SearchPage() {
-	let navigate = useNavigate();
-	const usernameTemp = useRef();
-	const tagTemp = useRef();
-	const { userData } = useContext(UserContext);
-	const { fetchData } = useContext(UserContext);
-	const { fetchDatammr } = useContext(UserContext);
-	const { fetchDatawin } = useContext(UserContext);
+  let navigate = useNavigate();
+  const usernameTemp = useRef();
+  const tagTemp = useRef();
+  const { userData } = useContext(UserContext);
+  const { fetchData } = useContext(UserContext);
+  const { fetchDatammr } = useContext(UserContext);
+  const { fetchDatawin } = useContext(UserContext);
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		userData.username = usernameTemp.current.value;
-		userData.tag = tagTemp.current.value;
-		fetchData();
-		fetchDatammr();
-		fetchDatawin();
-		navigate("/MainPage");
-	}
+  function handleSubmit(e) {
+    e.preventDefault();
+    userData.username = usernameTemp.current.value;
+    userData.tag = tagTemp.current.value;
+    fetchData();
+    fetchDatammr();
+    fetchDatawin();
+    navigate("/MainPage");
+  }
 
-	return (
-		<Flex
-			bgImage={wallpaper}
-			backgroundRepeat="no-repeat"
-			backgroundSize="cover"
-			overflow="hidden"
-			h="100vh"
-			justifyContent="center"
-			alignItems="center"
-		>
-			<Flex
-				className="Login"
-				bgColor="#FFFFFF"
-				px="40px"
-				pt="65px"
-				pb="40px"
-				borderRadius="20px"
-				alignItems="center"
-				flexDirection="column"
-			>
-				<Heading color="black" mb={8} size="lg">
-					Sign In
-				</Heading>
-				<Flex
-					className="inputField"
-					as="form"
-					onSubmit={handleSubmit}
-					flexDirection="column"
-					gap={2}
-					pt={5}
-					pb={4}
-					alignItems="center"
-					color="#535353"
-				>
-					<Input
-						ref={usernameTemp}
-						borderRadius="7px"
-						bgColor="#EDEDED"
-						placeholder="USERNAME"
-						width="306px"
-						height="51px"
-						fontWeight="semibold"
-						fontSize="md"
-						_placeholder={{ color: "#535353" }}
-					></Input>
-					<Input
-						ref={tagTemp}
-						borderRadius="7px"
-						bgColor="#EDEDED"
-						placeholder="TAGLINE"
-						width="306px"
-						height="51px"
-						fontWeight="semibold"
-						fontSize="md"
-						_placeholder={{ color: "#535353" }}
-					></Input>
-					<Flex className="ButtonGroup" justifyContent="space-between" w="100%">
-						<Button
-							bgColor="#D13639"
-							borderRadius="7px"
-							w="98px"
-							h="34px"
-							_hover={{ bgColor: "#D13639" }}
-							color="white"
-						>
-							<SiRiotgames />
-						</Button>
-						<Button bgColor="#E2E8F0" borderRadius="7px" w="98px" h="34px">
-							<FcGoogle />
-						</Button>
-						<Button
-							bgColor="black"
-							borderRadius="7px"
-							w="98px"
-							h="34px"
-							_hover={{ bgColor: "black" }}
-							color="white"
-						>
-							<SiApple />
-						</Button>
-					</Flex>
-					<Button
-						type="submit"
-						bgColor="#D13639"
-						mt="60px"
-						borderRadius="18px"
-						w="60px"
-						h="60px"
-						color="white"
-						_hover={{ color: "black", bgColor: "#E2E8F0" }}
-					>
-						<MdArrowForwardIos size="30px" />
-					</Button>
-				</Flex>
-				<Text
-					letterSpacing="tight"
-					fontWeight="bold"
-					color="#535353"
-					mt={6}
-					fontSize="sm"
-				>
-					GET HELP?
-				</Text>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      bgImage={wallpaper}
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      overflow="hidden"
+      h="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex
+        className="Login"
+        bgColor="#FFFFFF"
+        px="40px"
+        pt="65px"
+        pb="40px"
+        borderRadius="20px"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Heading color="black" mb={8} size="lg">
+          Sign In
+        </Heading>
+        <Flex
+          className="inputField"
+          as="form"
+          onSubmit={handleSubmit}
+          flexDirection="column"
+          gap={2}
+          pt={5}
+          pb={4}
+          alignItems="center"
+          color="#535353"
+        >
+          <Input
+            ref={usernameTemp}
+            borderRadius="7px"
+            bgColor="#EDEDED"
+            placeholder="USERNAME"
+            width="306px"
+            height="51px"
+            fontWeight="semibold"
+            fontSize="md"
+            _placeholder={{ color: "#535353" }}
+          ></Input>
+          <Input
+            ref={tagTemp}
+            borderRadius="7px"
+            bgColor="#EDEDED"
+            placeholder="TAGLINE"
+            width="306px"
+            height="51px"
+            fontWeight="semibold"
+            fontSize="md"
+            _placeholder={{ color: "#535353" }}
+          ></Input>
+          <Flex className="ButtonGroup" justifyContent="space-between" w="100%">
+            <Button
+              bgColor="#D13639"
+              borderRadius="7px"
+              w="98px"
+              h="34px"
+              _hover={{ bgColor: "#D13639" }}
+              color="white"
+            >
+              <SiRiotgames />
+            </Button>
+            <Button bgColor="#E2E8F0" borderRadius="7px" w="98px" h="34px">
+              <FcGoogle />
+            </Button>
+            <Button
+              bgColor="black"
+              borderRadius="7px"
+              w="98px"
+              h="34px"
+              _hover={{ bgColor: "black" }}
+              color="white"
+            >
+              <SiApple />
+            </Button>
+          </Flex>
+          <Button
+            type="submit"
+            bgColor="#D13639"
+            mt="60px"
+            borderRadius="18px"
+            w="60px"
+            h="60px"
+            color="white"
+            _hover={{ color: "black", bgColor: "#E2E8F0" }}
+          >
+            <MdArrowForwardIos size="30px" />
+          </Button>
+        </Flex>
+        <Text
+          letterSpacing="tight"
+          fontWeight="bold"
+          color="#535353"
+          mt={6}
+          fontSize="sm"
+        >
+          GET HELP?
+        </Text>
+      </Flex>
+    </Flex>
+  );
 }

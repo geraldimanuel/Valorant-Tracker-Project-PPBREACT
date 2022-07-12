f1e93ff7126a8442f13f6bea186fbfe81ebdfbaf
Jefer Setiawan <jefersetiawan04@gmail.com>
2022-07-12 11:13:29 +0700

    Add RR data

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -1,69 +1,70 @@
 import {
-	Heading,
-	Flex,
-	Text,
-	Box,
-	Image,
-	useColorModeValue,
+  Heading,
+  Flex,
+  Text,
+  Box,
+  Image,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { React, useContext, useEffect } from "react";
 import { BsFillTriangleFill } from "react-icons/bs";
 import { UserContext } from "../lib/UserContext";
 
 export default function Rank() {
-	const bg = useColorModeValue("#FFFFFF", "#121212");
-	const { mmr } = useContext(UserContext);
-	const { rankChecker } = useContext(UserContext);
-	const { rankurl } = useContext(UserContext);
+  const bg = useColorModeValue("#FFFFFF", "#121212");
+  const { mmr } = useContext(UserContext);
+  const { rankChecker } = useContext(UserContext);
+  const { rankurl } = useContext(UserContext);
 
-	useEffect(() => {
-		if (mmr.data != null) rankChecker();
-	}, [mmr]);
+  useEffect(() => {
+    if (mmr.data != null) rankChecker();
+  }, [mmr]);
 
-	return (
-		<Flex
-			bgColor={bg}
-			borderRadius="20px"
-			className="current-rank"
-			w={{ md: "50%", base: "100%" }}
-			p={{ md: 10, base: 5 }}
-			flexDirection="column"
-		>
-			<Heading size="sm" color="#AAB5B3">
-				Current rank
-			</Heading>
-			<Flex
-				h="100%"
-				alignItems="center"
-				justifyContent={{ md: "space-evenly", base: "space-around" }}
-			>
-				<Flex
-					flexDirection="column"
-					w={{ md: "60%", base: null }}
-					alignItems="center"
-					justifyContent="center"
-				>
-					<Flex alignItems="center" gap={3}>
-						<BsFillTriangleFill color="#46B8A3" />
-						<Flex flexDirection="column">
-							{mmr.data ? (
-								<Heading color="#46B8A3" size={{ md: "xl", base: "lg" }}>
-									{mmr.data.currenttierpatched}
-								</Heading>
-							) : null}
-
-							<Text color="#46B8A3" as="i">
-								200 RR
-							</Text>
-						</Flex>
-					</Flex>
-				</Flex>
-				<Flex w="40%" justifyContent="center">
-					{mmr.data ? (
-						<Image src={rankurl} boxSize={{ md: "100px", base: "80px" }} />
-					) : null}
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      bgColor={bg}
+      borderRadius="20px"
+      className="current-rank"
+      w={{ md: "50%", base: "100%" }}
+      p={{ md: 10, base: 5 }}
+      flexDirection="column"
+    >
+      <Heading size="sm" color="#AAB5B3">
+        Current rank
+      </Heading>
+      <Flex
+        h="100%"
+        alignItems="center"
+        justifyContent={{ md: "space-evenly", base: "space-around" }}
+      >
+        <Flex
+          flexDirection="column"
+          w={{ md: "60%", base: null }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Flex alignItems="center" gap={3}>
+            <BsFillTriangleFill color="#46B8A3" />
+            <Flex flexDirection="column">
+              {mmr.data ? (
+                <Heading color="#46B8A3" size={{ md: "xl", base: "lg" }}>
+                  {mmr.data.currenttierpatched}
+                </Heading>
+              ) : null}
+              {mmr.data ? (
+                <Text color="#46B8A3" as="i">
+                  {mmr.data.ranking_in_tier} RR
+                </Text>
+              ) : null}
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex w="40%" justifyContent="center">
+          {mmr.data ? (
+            <Image src={rankurl} boxSize={{ md: "100px", base: "80px" }} />
+          ) : null}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
